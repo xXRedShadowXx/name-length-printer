ae4978b3fb54c72f15cd6db2a1a2bc63c6d12421
Minecon2016 <49244146+Minecon2016@users.noreply.github.com>
2019-04-03 08:43:34 -0700

    Add files via upload

---
+++ NewCode.java
@@ -0,0 +1,16 @@
+
+import java.util.*;
+
+public class NewCode
+{
+    public static void main(String[] args)
+    {
+        
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("What is the biggest word you can think of?");
+        String word = input.nextLine();
+        int num = word.length();
+        System.out.println("that word has" +num+ "letters in it good job.")
+    }
+}
